c70d1f8f4670f2f9b4fddca6e86c237221660699
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-14 00:24:15 +0900

    optimization

---
+++ optimization/pages/events/index.js
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import Head from "next/head";
+
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>All Events</title>
+        <mete
+          name="description"
+          content="Find a lot of greate events that allow you to evolve..."
+        />
+      </Head>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
